d370234561e20e2bb700ca14e76f925f49cd83e3
GrooveypenguinX <jakechase2110@gmail.com>
2024-04-25 00:20:59 -0400

    new source for the voices

---
+++ VoicePatch/YojenkzHeadsVoiceAdder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using BepInEx;
+using UnityEngine;
+
+namespace YojenkzHeadsVoiceAdder
+{
+
+    [BepInPlugin("com.grooveypenguinx.basedonCWXVoiceAdder.WTT-TheLongLostHeadsOfYojenkz", "WTT-TheLongLostHeadsOfYojenkz", "0.6.9")]
+    public class YojenkzHeadsVoiceAdder : BaseUnityPlugin
+    {
+
+        private void Start()
+        {
+            new YojenkHeadsVoicePatch().Enable();
+        }
+    }
+}
